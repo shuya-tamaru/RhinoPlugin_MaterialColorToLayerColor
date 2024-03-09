40bc305aa009a59d5d316da2dcbeb59e6917e350
shuya-yamaru <tamaru1013@gmail.com>
2024-03-09 10:14:48 +0900

    プロジェクト ファイルを追加します。

---
+++ MaterialColorToLayerColorCommand.cs
@@ -0,0 +1,48 @@
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+using System;
+using System.Collections.Generic;
+
+namespace MaterialColorToLayerColor
+{
+    public class MaterialColorToLayerColorCommand : Command
+    {
+        public MaterialColorToLayerColorCommand()
+        {
+            Instance = this;
+        }
+
+        public static MaterialColorToLayerColorCommand Instance { get; private set; }
+
+        public override string EnglishName => "MaterialColorToLayerColor";
+
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            var layers = doc.Layers;
+            foreach (var layer in layers)
+            {
+                int materialIndex = layer.RenderMaterialIndex;
+                if (materialIndex != -1)
+                {
+                    var material = doc.Materials[materialIndex];
+                    if (material != null)
+                    {
+                        var materialColor = material.DiffuseColor;
+                        layer.Color = materialColor;
+                        RhinoApp.WriteLine("レイヤー '{0}' の色を {1} に設定しました。", layer.Name, materialColor.ToString());
+                    }
+                }
+                else
+                {
+                    RhinoApp.WriteLine("レイヤー '{0}' にはマテリアルが設定されていません。", layer.Name);
+                }
+            }
+            doc.Views.Redraw();
+            return Result.Success;
+        }
+    }
+}
